d8cbab8ad4d1013e08e816044eb77e8054f2b457
Marie <marie.marcos25@gmail.com>
2021-08-11 21:27:09 -0700

    added working autocorrect test

---
+++ main/auto_correct.py
@@ -0,0 +1,12 @@
+# Working test of textblob
+# https://www.geeksforgeeks.org/spelling-checker-in-python/
+
+from textblob import TextBlob
+ 
+message = "Hello confsion houes"
+print("entered: "+str(message))
+ 
+corrected = TextBlob(message)
+ 
+# prints the corrected spelling
+print("corrected: "+str(corrected.correct()))
