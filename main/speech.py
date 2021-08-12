15f0a120182fa360e53f2dc0feb2c92dce591305
Edward Regalado <tregalado30@gmail.com>
2021-08-11 22:24:23 -0700

    added readme info

--- main/speech.py
+++ main/speech.py
@@ -1,4 +1,25 @@
 # import SpeechRecognition
+import speech_recognition as sr
+
+recognizer = pyaudio.Recognizer()
 
 def speech_to_text():
-    pass
+    while True:
+        
+        try:
+            with recognizer.Microphone() as mic:
+            
+                recognizer.adjust_for_ambient_noise(mic, duration = 0.2)
+                audio = recognizer.listen(mic)
+                
+                text = recognizer.recognize_google(audio)
+                text = text.lower()
+                
+                print(f"Recognized {text}")
+                
+        except recognizer.UknownValueError():
+            
+            recognizer = recognizer.Recognizer()
+            continue
+        
+        
