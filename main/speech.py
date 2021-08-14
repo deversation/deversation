90094b040259cb2ba75f4f5e4944d2bf2eec6ae8
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2021-08-14 11:00:49 -0500

    local changes, Troubleshooting Type to speech

--- main/speech.py
+++ main/speech.py
@@ -1,24 +1,40 @@
-import SpeechRecognition as speech_recognition
+# import SpeechRecognition as speech_recognition
+from gtts import gTTS
+import os
+
+fh = open('test.txt', 'r')
+my_text = fh.read().replace('\n', ' ')
+language = 'en'
+output = gTTS(text=my_text, lang=language, slow=False)
+output.save("attempt_1.mp3")
+fh.close()
+os.system("start attempt_1.mp3")
+
+
 # import pyaudio
 # import pyflac
 
 
 
-recognizer = speech_recognition.Recognizer()
+# recognizer = speech_recognition.Recognizer()
+
+
+
 
 
 def speech_to_text():
-    while True:
-        try:
-            with speech_recognition.Microphone() as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration = 0.2)
-                audio = recognizer.listen(mic)
-                text = recognizer.recognize_google(audio)
-                text = text.lower()
-                print(f"Recognized {text}")
-        except speech_recognition.UknownValueError():
-            recognizer = speech_recognition.Recognizer()
-            continue
+    pass
+    # while True:
+    #     try:
+    #         with speech_recognition.Microphone() as mic:
+    #             recognizer.adjust_for_ambient_noise(mic, duration = 0.2)
+    #             audio = recognizer.listen(mic)
+    #             text = recognizer.recognize_google(audio)
+    #             text = text.lower()
+    #             print(f"Recognized {text}")
+    #     except speech_recognition.UknownValueError():
+    #         recognizer = speech_recognition.Recognizer()
+    #         continue
 
 
-speech_to_text()
+# speech_to_text()
